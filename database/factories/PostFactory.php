008ef9dc0eeca1b130d90a4884191ad42ea9b9a8
Pete Waters <pete.d.waters@gmail.com>
2017-08-29 20:56:23 +0000

    Created Post factory and faked up some posts

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Post;
+
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(1),
+        'body' => $faker->paragraph(3)
+    ];
+});
