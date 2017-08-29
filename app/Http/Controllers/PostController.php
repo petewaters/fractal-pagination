822778c7a5566ed5ffa24e6e582034b6ae4b0840
Pete Waters <pete.d.waters@gmail.com>
2017-08-29 21:16:04 +0000

    Updated post controller index method to paginate posts passed to fractal

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,17 +5,19 @@ namespace App\Http\Controllers;
 use App\Post;
 use App\Transformers\PostTransformer;
 
+use League\Fractal\Pagination\IlluminatePaginatorAdapter;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        $posts = $post->latestFirst()->get();
+        $posts = $post->latestFirst()->paginate(10);
 
         return fractal()
             ->collection($posts)
             ->parseIncludes(['user'])
-            ->transformWith(new PostTransformer);
+            ->transformWith(new PostTransformer)
+            ->paginateWith(new IlluminatePaginatorAdapter($posts));
     }
 }
